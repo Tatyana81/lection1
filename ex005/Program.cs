1a5966912f9947d31d1f4357b88e354410fc27de
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-07-10 12:23:47 +0700

    ex005

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string Username = Console.ReadLine();
+
+if (Username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же Маша!!!");
+}
+else
+{
+     Console.Write ("Привет, ");
+     Console.WriteLine (Username);
+}
